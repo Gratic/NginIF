11a7065f6734d1c12543ee5e203aca3b4c5f84d6
Florie <gvillegas34@hotmail.com>
2021-11-17 12:00:15 +0100

    Test image

--- src/http/server/methods/GetRequest.java
+++ src/http/server/methods/GetRequest.java
@@ -1,30 +1,59 @@
 package http.server.methods;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.PrintWriter;
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.*;
 import java.util.Map;
 
 public class GetRequest implements Method {
     @Override
     public void processMethod(Map<String, Object> request, PrintWriter out, BufferedReader in) {
+
+        String resource = (String)request.get("resource");
+        System.out.println(resource);
+        String type ="text/html";
+        String[] splitResource = resource.split("\\.");
+        switch (splitResource[1]){
+            case "html" -> {
+                type = "text/html";
+            }
+            case "png" -> {
+                type = "image/png";
+            }
+        }
         out.println("HTTP/1.0 200 OK");
-        out.println("Content-Type: text/html");
+        out.println("Content-Type: "+type);
         out.println("Server: Bot");
-        // this blank line signals the end of the headers
         out.println("");
         // Send the HTML page
-        try {
-            BufferedReader reader = new BufferedReader(new FileReader("resources" + request.get("resource")));
-            String currentLine;
-            while ((currentLine = reader.readLine()) != null) {
-                out.println(currentLine);
-            }
+        switch (type)
+        {
+            case "text/html" ->
+                {
+                    try {
+                        BufferedReader reader = new BufferedReader(new FileReader("resources" + request.get("resource")));
+                        String currentLine;
+                        while ((currentLine = reader.readLine()) != null) {
+                            out.println(currentLine);
+                        }
 
-            reader.close();
-        } catch (IOException e) {
-            e.printStackTrace();
+                        reader.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            case "image/png" ->
+                {
+                    try {
+                        //BufferedInputStream reader = new BufferedInputStream(new FileReader("resources" + request.get("resource")));
+                        BufferedImage image = ImageIO.read(new File("resources" + request.get("resource")));
+                        out.print(image);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+
         }
+
     }
 }
