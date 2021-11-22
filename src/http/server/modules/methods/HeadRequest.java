bc7053525d32134440f96b36e80415260bbf6d07
Gratic <playerzaleks@gmail.com>
2021-11-22 15:37:31 +0100

    remove todo

--- src/http/server/modules/methods/HeadRequest.java
+++ src/http/server/modules/methods/HeadRequest.java
@@ -13,11 +13,6 @@ public class HeadRequest implements Method {
     public void processMethod(HttpHeader header, BufferedReader input, OutputStream outputStream) {
         File f = new File("resources" + header.getResource());
 
-        if (header.isServlet()) {
-            // TODO: faifrjezojn,rkldsnf,kloqdsin,fdiopsn,fikoer
-            // TODO: faifrjezojn,rkldsnf,kloqdsin,fdiopsn,fikoer
-        }
-
         ResponseHttpHeader responseHttpHeader = GetRequest.getHeader(HttpStatusCode.OK_200, f.length(), header.getContentType());
         responseHttpHeader.write(outputStream);
     }
